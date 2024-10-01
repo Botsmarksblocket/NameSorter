3115282ee0632e77ee41629a470fae59fcb02ee0
Edvard <edde113@hotmail.com>
2024-10-01 09:21:16 +0200

    Allows user to enter a name

---
+++ NameSorter/InputMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NameSorter
+{
+    class InputMethods
+    {
+        static List<string> names = new List<string>() { "Anna", "John", "Alice", "Bob", "James", "Jenny" }; //Creates a list that contains a few names
+
+        public static string EnterName(string prompt) //Allows user to enter a name
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine().Trim(); //Removes all white-space before and after the string
+            Console.Clear();
+
+            return input;            
+        }
+
