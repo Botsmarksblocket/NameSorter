f6ae093b6d8e8242ca8db1256b8008a2fe4e0453
Edvard <edde113@hotmail.com>
2024-10-01 09:25:06 +0200

    Do-while loop as a menu

---
+++ NameSorter/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSorter
+{
+    static class Menu
+    {
+        public static void Run()
+        {
+            string name;
+            string finalName;
+            string userInput;
+            do
+            {
+                Console.WriteLine("Enter your choice:");
+                Console.WriteLine("------------------\n");
+                Console.WriteLine("Add: Add a name to the list.");
+                Console.WriteLine("Remove: Remove a name from the list.");
+                Console.WriteLine("Print: Print the list.");
+                Console.WriteLine("Sort: Sort the list.");
+                Console.WriteLine("Search: Search the list for a specific name.");
+                Console.WriteLine("Exit: Exit the program.");
+
+                userInput = Console.ReadLine().ToLower();
+
+                Console.Clear();
+                switch (userInput)
+            } while (userInput != "exit");
+        }
+    }
+}
